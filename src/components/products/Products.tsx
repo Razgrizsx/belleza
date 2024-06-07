afbf4d9c9399923a5a9a87629ab7ddd043dfdb66
Razgrizsx <razgrisss41@gmail.com>
2024-06-07 07:54:05 -0300

    key

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -10,7 +10,10 @@ export const Products = ({ products }: Props) => {
   return (
     <div className='grid grid-cols-4 gap-2 w-5/6 rounded-md'>
       {products.map((elem: Product) => (
-        <div className='z-50 p-2 group cursor-pointer rounded-md'>
+        <div
+          key={elem.name}
+          className='z-50 p-2 group cursor-pointer rounded-md'
+        >
           <div className='relative '>
             <div className='absolute rounded-md group circle flex items-center justify-center top-0 left-0 bg-pink opacity-0 group-hover:opacity-30 transition ease-in-out w-full h-full z-40'></div>
             <div className='absolute  flex items-center justify-center top-[calc(50%-50px)] left-[calc(50%-50px)] bg-white rounded-full ease-in-out w-[0px] h-[0px] group-hover:scale-100 group-hover:w-[100px] group-hover:h-[100px] transform transition-all duration-300 hover:bg-pink z-50'>
