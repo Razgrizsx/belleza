f8e3d5366ecdb17ff66b29f2c70d9804c78fd088
Razgrizsx <razgrisss41@gmail.com>
2024-06-08 13:35:36 -0300

    contacto, carousel

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,39 +1,112 @@
+"use client";
+
 import { FaFacebook, FaInstagram } from "react-icons/fa";
+import ReCAPTCHA from "react-google-recaptcha";
+import { useState } from "react";
 
 export default function Contacto() {
+  const [capVal, setCapVal] = useState<string | null>(null);
+
   return (
-    <main className='flex items-center justify-center my-14 flex-col mx-20'>
+    <main className='flex items-center justify-center my-14 flex-col '>
       <div className='m-2 px-8 w-full mx-8 flex flex-col gap-3 justify-center items-center text-purple font-poppins pb-36'>
-        <div className='font-bold text-2xl pb-2'>CONTACTO</div>
+        <div className='font-bold text-[36px] pb-2'>CONTACTO</div>
         <div className='flex flex-col w-full gap-6'>
           <div className='text-lg text-center'>
             PARA PRESUPUESTOS, CONSULTAS Y COMENTARIOS NO DUDE EN ENVIARNOS UN
             MENSAJE
           </div>
-          <div className='flex flex-row w-full text-lg text-center gap-2 justify-center'>
-            <div className='w-1/3'>
-              <div>NUESTRA CENTRAL</div>
-              <div>Pasteur 127 · Capital Federal</div>
-              <div>Buenos Aires · Argentina</div>
+          <div className='flex flex-row w-full text-lg text-center gap-16 justify-center font-poppins'>
+            <div className='flex flex-col gap-9 w-1/3 items-start justify-start'>
+              <div className='text-base'>NUESTRA CENTRAL</div>
+              <div className='text-[26px]'>
+                <div className='flex flex-row font font-semibold text-pink'>
+                  Pasteur 127
+                  <p className='pl-1 font-normal text-pink'>
+                    · Capital Federal
+                  </p>
+                </div>
+                <div className='flex items-start text-[#4d4d4d]'>
+                  Buenos Aires · Argentina
+                </div>
+              </div>
               <div>NUESTRA LÍNEA</div>
-              <div>(011) 4952-7534</div>
+              <div className='text-[26px] text-[#4d4d4d]'>(011) 4952-7534</div>
               <div>WHATSAPP</div>
-              <div>+54 9 11 4069 6478</div>
-              <div className='flex flex-row'>
+              <div className='text-[26px] text-[#337ab7] cursor-pointer hover:underline'>
+                +54 9 11 4069 6478
+              </div>
+              <div className='flex flex-row gap-9'>
                 <div>
-                  <div>EMAIL</div>
-                  <div>casayulisrl@gmail.com</div>
+                  <div className='text-start text-[16px]'>EMAIL</div>
+                  <div className='text-[24px] text-[#4d4d4d] cursor-pointer pt-2 hover:text-pink'>
+                    casayulisrl@gmail.com
+                  </div>
                 </div>
                 <div>
-                  <div>NUESTRAS REDES</div>
-                  <div>
-                    <FaFacebook />
-                    <FaInstagram />
+                  <div className='text-start text-[16px] pb-3'>
+                    NUESTRAS REDES
+                  </div>
+                  <div className='flex flex-row gap-2'>
+                    <FaFacebook className='fill-[#8d8d8d] hover:fill-[#c7298e] cursor-pointer w-6 h-6' />
+                    <FaInstagram className='fill-[#8d8d8d] hover:fill-[#c7298e] cursor-pointer w-6 h-6' />
                   </div>
                 </div>
               </div>
             </div>
-            <div className='w-1/3'>2</div>
+            <div className='w-1/3 flex flex-col gap-2'>
+              <input
+                type='text'
+                placeholder='NOMBRE'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='APELLIDO'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='MAIL'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='TELEFONO'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='DIRECCION'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='CUIT'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <input
+                type='text'
+                placeholder='TRANSPORTE'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2'
+              />
+              <textarea
+                placeholder='MENSAJE'
+                className='bg-[#ebebeb] border-none outline-none text-[12px] p-1 px-2 h-24'
+              />
+              <ReCAPTCHA
+                sitekey='6LfmIPQpAAAAAPZ6V6J1ID8ujpIzUyuYr_VqLUXq'
+                onChange={(val) => {
+                  setCapVal(val);
+                }}
+              />
+              <button
+                className='w-fit rounded-md bg-pink p-2 px-14 mt-3 border border-[#c7298e] text-[14px] hover:bg-white text-white hover:text-pink'
+                disabled={!capVal}
+              >
+                ENVIAR
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/carrousel/Carousel.tsx
+++ src/components/carrousel/Carousel.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { FaAngleLeft } from "react-icons/fa";
-import { FaAngleRight } from "react-icons/fa";
+import {
+  FaAngleLeft,
+  FaAngleRight,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaEnvelope
+} from "react-icons/fa";
 
 import "./Carousel.css";
 
@@ -46,20 +51,44 @@ export const Carousel = ({
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
           {images.map((image, index) => (
-            <div className='carousel-item' key={index}>
+            <div className='carousel-item relative' key={index}>
+              <div className='absolute flex flex-col gap-[1px] left-[15%] w-[230px] h-[200px] top-[10%]'>
+                <div className='bg-white text-purple w-full pb-2 h-[55%] rounded-t-md flex flex-col items-center justify-center'>
+                  <div className='text-[38px] flex flex-row'>
+                    casa <p className='font-semibold pl-3'>yuli</p>
+                  </div>
+                  <p className='text-center text-sm leading-[15px]'>
+                    distribuidora mayorista de cosméticos y perfumería
+                  </p>
+                </div>
+                <div className='bg-purple text-sm text-white w-full h-[45%] rounded-b-md flex flex-col items-start pl-5 justify-center'>
+                  <div className='flex flex-row items-center justify-center'>
+                    <FaMapMarkerAlt className='fill-[#e38db2] w-3 h-3' />
+                    <p className='pl-2'>Pasteur 127 - CABA</p>
+                  </div>
+                  <div className='flex flex-row items-center justify-center'>
+                    <FaPhoneAlt className='fill-[#e38db2] w-3 h-3' />
+                    <p className='pl-2'>4952-7534</p>
+                  </div>
+                  <div className='flex flex-row items-center justify-center'>
+                    <FaEnvelope className='fill-[#e38db2] w-3 h-3' />
+                    <p className='pl-2'>casayuli@gmail.com</p>
+                  </div>
+                </div>
+              </div>
               <img src={image} alt={`Slide ${index}`} />
             </div>
           ))}
         </div>
         <button
           onClick={goToPrevious}
-          className='carousel-control prev flex items-center justify-center'
+          className='carousel-control prev flex items-center justify-center hover:bg-gradient-to-r hover:from-[#8a538a]'
         >
           <FaAngleLeft className='h-12 w-12 opacity-50' />
         </button>
         <button
           onClick={goToNext}
-          className='carousel-control next flex items-center justify-center'
+          className='carousel-control next flex items-center justify-center hover:bg-gradient-to-l hover:from-[#8a538a]'
         >
           <FaAngleRight className='h-12 w-12 opacity-50' />
         </button>
